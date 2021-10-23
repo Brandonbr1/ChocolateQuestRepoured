105f1efc002cd41857249f04cce761504a0b27ac
DerToaster98 <seiler.oli@web.de>
2021-10-23 12:35:24 +0200

    create config classes for each boss

---
+++ src/main/java/team/cqr/cqrepoured/config/boss/implementations/BossConfigEnderKing.java
@@ -0,0 +1,7 @@
+package team.cqr.cqrepoured.config.boss.implementations;
+
+import team.cqr.cqrepoured.config.boss.AbstractBossConfig;
+
+public class BossConfigEnderKing extends AbstractBossConfig<T> {
+
+}
