b9af654a0cb90f2c4ef24638c2c81c70042acbcd
DerToaster98 <seiler.oli@web.de>
2021-10-23 13:22:43 +0200

    add common default values

--- src/main/java/team/cqr/cqrepoured/config/boss/implementations/BossConfigBoarmage.java
+++ src/main/java/team/cqr/cqrepoured/config/boss/implementations/BossConfigBoarmage.java
@@ -5,4 +5,60 @@ import team.cqr.cqrepoured.objects.entity.boss.EntityCQRBoarmage;
 
 public class BossConfigBoarmage extends AbstractBossConfig<EntityCQRBoarmage> {
 
+	//These are all the same accross all bosses
+	@Override
+	protected double getStandardAttackDamageDefaultValue() {
+		return 2.0D;
+	}
+	
+	@Override
+	protected double getBaseSpeedDefaultValue() {
+		return 0.25D;
+	}
+
+	@Override
+	protected double getArmorValueDefaultValue() {
+		return 0;
+	}
+
+	@Override
+	protected double getArmorToughnessDefaultValue() {
+		return 0;
+	}
+
+	@Override
+	protected double getKnockbackResistanceDefaultValue() {
+		return 0;
+	}
+
+	@Override
+	protected boolean isDamageCapEnabledDefaultValue() {
+		return true;
+	}
+
+	@Override
+	protected float getMaxUncappedDamageDefaultValue() {
+		return 30F;
+	}
+
+	@Override
+	protected float getMaxDamageInPercentOfMaxHPDefaultValue() {
+		return 0.1F;
+	}
+
+	@Override
+	protected double getDamageReductionPerPlayerDefaultValue() {
+		return 0.25D;
+	}
+
+	@Override
+	protected boolean isBossBarEnabledDefaultValue() {
+		return true;
+	}
+
+	@Override
+	protected boolean isHealthRegenEnabledDefaultValue() {
+		return true;
+	}
+
 }
--- src/main/java/team/cqr/cqrepoured/config/boss/implementations/BossConfigWalkerKing.java
+++ src/main/java/team/cqr/cqrepoured/config/boss/implementations/BossConfigWalkerKing.java
@@ -6,4 +6,59 @@ import team.cqr.cqrepoured.objects.entity.mobs.EntityCQRWalker;
 
 public class BossConfigWalkerKing extends AbstractBossConfig<EntityCQRWalkerKing> {
 
+	//These are all the same accross all bosses
+		@Override
+		protected double getStandardAttackDamageDefaultValue() {
+			return 2.0D;
+		}
+		
+		@Override
+		protected double getBaseSpeedDefaultValue() {
+			return 0.25D;
+		}
+
+		@Override
+		protected double getArmorValueDefaultValue() {
+			return 0;
+		}
+
+		@Override
+		protected double getArmorToughnessDefaultValue() {
+			return 0;
+		}
+
+		@Override
+		protected double getKnockbackResistanceDefaultValue() {
+			return 0;
+		}
+
+		@Override
+		protected boolean isDamageCapEnabledDefaultValue() {
+			return true;
+		}
+
+		@Override
+		protected float getMaxUncappedDamageDefaultValue() {
+			return 30F;
+		}
+
+		@Override
+		protected float getMaxDamageInPercentOfMaxHPDefaultValue() {
+			return 0.1F;
+		}
+
+		@Override
+		protected double getDamageReductionPerPlayerDefaultValue() {
+			return 0.25D;
+		}
+
+		@Override
+		protected boolean isBossBarEnabledDefaultValue() {
+			return true;
+		}
+
+		@Override
+		protected boolean isHealthRegenEnabledDefaultValue() {
+			return true;
+		}
 }
